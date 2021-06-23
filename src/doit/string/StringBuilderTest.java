78addf23d9710c5dce4fc8107c37af10172f1110
bell-ho <jj0101065@gmail.com>
2021-06-23 23:20:56 +0900

    DO IT JAVA String 374

---
+++ src/doit/string/StringBuilderTest.java
@@ -0,0 +1,20 @@
+package doit.string;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        String javaStr = new String("Java");
+        System.out.println("문자열 주소 : " + System.identityHashCode(javaStr));
+
+        StringBuilder buffer = new StringBuilder(javaStr);
+        System.out.println("연산전" + System.identityHashCode(buffer));
+
+        buffer.append(" and");
+        buffer.append(" android");
+
+        System.out.println("연산후" + System.identityHashCode(buffer));
+
+        javaStr = buffer.toString();
+        System.out.println(javaStr);
+        System.out.println("연결된 " + System.identityHashCode(javaStr));
+    }
+}
