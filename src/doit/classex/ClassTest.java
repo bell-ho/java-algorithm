0d52699516c7d7eecf98893a915d1589f88cc2d4
DESKTOP-RLMN7MO\admin <jh01010@doctorsoft.co.kr>
2021-06-24 16:10:09 +0900

    Do it Java 클래스 386

---
+++ src/doit/classex/ClassTest.java
@@ -0,0 +1,16 @@
+package doit.classex;
+
+public class ClassTest {
+    public static void main(String[] args) throws ClassNotFoundException{
+        Person p1 = new Person();
+        Class pClass1 = p1.getClass();
+        System.out.println(pClass1.getName());
+
+        Class pClass2 = Person.class;
+        System.out.println(pClass2.getName());
+
+        Class pClass3 = Class.forName("doit.classex.Person");
+        System.out.println(pClass3.getName());
+
+    }
+}
